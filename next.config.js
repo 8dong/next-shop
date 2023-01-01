4420838ee5a82a0afe138586914c77ee4c91a206
DongHyun <hyde981114@gmail.com>
2023-01-01 22:19:57 +0900

    feat: CORS Proxy 설정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['cdn.class101.net']
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'http://shopitemapi-env.eba-zmpmdkiy.ap-northeast-2.elasticbeanstalk.com/'
+      }
+    ];
   }
 };
 
